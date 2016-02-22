33d77bd7d8af148625e9bb919bc51e3ef7481304
Sebastien Dubois <sdubois@protonmail.com>
2016-02-22 14:28:08 +0200

    Add timer control buttons

--- app/components/timer/Timer.jsx
+++ app/components/timer/Timer.jsx
@@ -1,30 +1,72 @@
 import React from 'react';
+import Radium from 'radium';
 import bellFile from './bell.wav';
 
 var bell = new Audio(bellFile);
+var myState = {};
 
+@Radium
 export default class Timer extends React.Component {
-  checkTime() {
-    this.setState({ remainingSeconds: this.remainingSeconds-- });
 
-    if(this.remainingSeconds < 0) {
-      window.clearInterval(this.timer);
+  checkTime() {
+    this.setState(myState);
+    myState.remainingSeconds--;
+
+    if(myState.remainingSeconds < 0) {
+      this.stop();
       bell.play();
     }
   }
 
-  componentWillMount() {
-    this.remainingSeconds = 10;
+  reset() {
+    this.stop();
+    myState.remainingSeconds = 5;
     this.checkTime();
   }
 
-  componentDidMount() {
+  start() {
+    myState.counting = true;
+    this.setState(myState);
     this.timer = window.setInterval(function () {
       this.checkTime();
     }.bind(this), 1000);
   }
 
+  stop() {
+    this.counting = false;
+    this.setState(myState);
+    window.clearInterval(this.timer);
+  }
+
+  componentWillMount() {
+    this.reset();
+  }
+
   render() {
-    return <div>{this.state.remainingSeconds}</div>;
+    const timeStyle = {
+      padding: '5px 5px 15px 5px'
+    };
+
+    const buttonStyle = {
+      fontSize: '18px',
+      border: 'none',
+      background: 'cornsilk',
+      cursor: 'pointer',
+      padding: '3px',
+      ':hover': {
+        background: 'rgb(150, 181, 180)'
+      }
+    };
+
+    return (
+      <div>
+        <div style={timeStyle}>{this.state.remainingSeconds}</div>
+        <div>
+          <button style={buttonStyle} disabled={this.state.counting} ref='reset' onClick={this.reset.bind(this)}>Reset</button>
+          <button style={buttonStyle} disabled={this.state.counting} ref='start' onClick={this.start.bind(this)}>Start</button>
+          <button style={buttonStyle} disabled={!this.state.counting} ref='stop' onClick={this.stop.bind(this)}>Stop</button>
+        </div>
+      </div>
+    );
   }
 }
