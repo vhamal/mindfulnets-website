13a6b438090f007197693fedd64ce8ff51e11595
Sebastien Dubois <sdubois@protonmail.com>
2016-03-13 22:03:34 +0200

    Add front-end router

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,11 +2,16 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'react-bootstrap';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+import App from './App';
 
 const app = document.createElement('div');
 
 document.body.appendChild(app);
 
-ReactDOM.render(<App />, app);
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+  </Router>
+), app);
