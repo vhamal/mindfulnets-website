1c06df2361c45ce961468030629724131ffaa611
Sebastien Dubois <sdubois@protonmail.com>
2016-03-21 17:35:38 +0200

    Add a debug log

--- frontend/src/components/practices.js
+++ frontend/src/components/practices.js
@@ -4,6 +4,7 @@ const PRACTICES_URL = `${process.env.BACKEND_URL}/api/practices`;
 
 module.exports = {
   put: body => {
+    console.log(`Calling PUT ${PRACTICES_URL} with ${JSON.stringify(body)}`);
     return fetch(PRACTICES_URL, {
       method: 'put',
       body: JSON.stringify(body)
